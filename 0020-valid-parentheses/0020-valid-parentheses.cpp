02b75c4650e71610b2538f33979f865e41e6e354
Harsh kumar <harshkumarsingh4343@gmail.com>
2026-01-18 20:02:42 +0530

    Time: 0 ms (100%), Space: 8.8 MB (86.96%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        // Best approach :> Stack(lifo)
+        stack<char> st;
+
+        for(char c : s){
+            if(c == '('|| c == '{' || c == '['){
+                st.push(c);
+
+            }else{
+                if(st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
