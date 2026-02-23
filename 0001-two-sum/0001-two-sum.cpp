5d34eb26517f29aedb4a436154adef97d5007246
Harsh kumar <harshkumarsingh4343@gmail.com>
2026-02-24 00:48:51 +0530

    Time: 3 ms (68.04%), Space: 14.8 MB (36.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
       //map use karenge 
-      unordered_map<int,int> mp;
-        for(int i = 0 ; i < nums.size(); i++){
-            int need = target - nums[i];
-            if(mp.count(need)){
+      unordered_map<int,int> mp; // map assign karenge
+        for(int i = 0 ; i < nums.size(); i++){  //loop iterate karenge array ke all element me
+            int need = target - nums[i];        // set target - nums[i] and search it in map 
+            if(mp.count(need)){ // check weather it is available in map and retrun its index and i ie index of current element
                 return{mp[need],i};
             }
-            mp[nums[i]]=i;
+            mp[nums[i]]=i; // if need is not available in map then insert it
         }  
         return{};
     }
