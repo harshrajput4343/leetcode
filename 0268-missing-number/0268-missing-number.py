91fb76209e5c6825c071adcaba8409a97c4c40b3
Harsh kumar <harshkumarsingh4343@gmail.com>
2026-03-13 20:08:08 +0530

    Time: 0 ms (100%), Space: 20.6 MB (18.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,15 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
+        return sum(range(len(nums) + 1)) - sum(nums)
+        #len(nums) + 1 -> (n + 1) so n tak ka sum - sum of nums
 
-        for i , v in enumerate(nums):
-            if (i != v):
-                return v - 1
 
-            elif (v == len(nums) - 1): # last element then next value is missing ie in n
-                return v + 1
+
+# nums.sort()
+
+#       for i , v in enumerate(nums):
+#            if (i != v): if missing in middle
+#                return v - 1
+
+#            elif (v == len(nums) - 1): # last element then next value is missing ie in n
+#                return v + 1
