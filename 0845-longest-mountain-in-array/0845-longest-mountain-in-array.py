1e2d7dfde6a6b892214e8888201ebdd4b73ad4eb
Harsh kumar <harshkumarsingh4343@gmail.com>
2026-03-15 23:04:41 +0530

    Time: 7 ms (96.39%), Space: 20.3 MB (85.79%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -2,17 +2,17 @@ class Solution:
     def longestMountain(self, arr: List[int]) -> int:
         result = 0
 
-        for i in range(1,len(arr)-1):
+        for i in range(1,len(arr)-1): # traversing from 2nd element to 2nd last element, ie. mountain peak can not be 1st and last  
 
-            if arr[i - 1] < arr[i] > arr[i + 1]:
+            if arr[i - 1] < arr[i] > arr[i + 1]:  # peak element
 
                 l=r=i
-                while l > 0 and arr[l]>arr[l-1]:
+                while l > 0 and arr[l] > arr[l-1]: # going left from peak
                     l-= 1
                 
-                while r < len(arr)-1 and arr[r] > arr[r+1]:
+                while r < len(arr)-1 and arr[r] > arr[r+1]: # going right from peak
                     r += 1
 
                 result = max(result,r-l+1)
-
+                
         return result
